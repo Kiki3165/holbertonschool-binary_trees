6b58d5bdc39d2171c5e4620d64f5ff20e1d329bd
kiki3165 <kikikou82@gmail.com>
2023-01-09 18:17:05 +0000

    change code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,28 +8,25 @@
 */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
-    if (parent == NULL) {
-        return NULL;
-    }
+  if (parent == NULL) {
+    return NULL;
+  }
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
-        return NULL;
-    }
+  binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+  if (new_node == NULL) {
+    return NULL;
+  }
 
-    new_node->value = value;
+  new_node->parent = parent;
+  new_node->value = value;
+  new_node->left = parent->left;
+  new_node->right = NULL;
 
-    new_node->left = NULL;
-    new_node->right = NULL;
+  if (parent->left != NULL) {
+    parent->left->parent = new_node;
+  }
 
-    new_node->parent = parent;
+  parent->left = new_node;
 
-    binary_tree_t *old_left_child = parent->left;
-    parent->left = new_node;
-
-    if (old_left_child != NULL) {
-        old_left_child->parent = new_node;
-        new_node->left = old_left_child;
-    }
-    return new_node;
+  return new_node;
 }
