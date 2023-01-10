8b270bc8d8bca588939d42096a6d58388b2b4cb1
kiki3165 <kikikou82@gmail.com>
2023-01-10 15:20:33 +0000

    fixvalgrind

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
     if (tree == NULL)
     {
         return (0);
     }
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
     return ((left_height > right_height ? left_height : right_height) + 1);
-    free(tree);
 }
