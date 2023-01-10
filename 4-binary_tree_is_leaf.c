af24ddebf6a8b1d11baa8c1280643707d25c6e7d
kiki3165 <kikikou82@gmail.com>
2023-01-10 14:29:37 +0000

    fix node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,17 +3,19 @@
 /**
 *binary_tree_is_leaf-binary_tree_is_leaf
 *@node:node
-*
+*Return:0
 */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-    return (0);
-        if (node->left == NULL && node->right == NULL) {
-        return (1);
-    }
-    else {
-        return (0);
-    }
+if (node == NULL)
+return (0);
+if (node->left == NULL && node->right == NULL)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
