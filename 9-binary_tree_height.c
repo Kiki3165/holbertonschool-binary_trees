631c1273010e8d38e3a5ac3a318bad601f56d98a
kiki3165 <kikikou82@gmail.com>
2023-01-10 15:18:18 +0000

    fixvalgrind

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
     {
         return (0);
-        free(tree);
     }
     return ((left_height > right_height ? left_height : right_height) + 1);
+    free(tree);
 }
